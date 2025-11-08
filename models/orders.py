2c8cf13c967bffcddf444f51ab3ffdc1d2b1679d
luciakasman <lucia.kasman@gmail.com>
2025-11-08 16:43:21 +0100

    change to english

--- models/orders.py
+++ models/orders.py
@@ -1,25 +1,24 @@
-
 from typing import List, Optional
 from dataclasses import dataclass
 
 @dataclass
 class OrderData:
     order_id: str
-    id_deposito: str
+    warehouse_id: str
     sku: str
-    cantidad_kg_litros: float
-    nombre_cliente: Optional[str] = None
-    nombre_producto: Optional[str] = None
+    quantity_kg_liters: float
+    customer_name: Optional[str] = None
+    product_name: Optional[str] = None
     total_usd: Optional[float] = None
     pallets: float = 0
 
 @dataclass
 class AvailableVehiclesData:
-    nombre_empresa: str
+    company_name: str
     SEMIS: int = 0
     BALANCIN: int = 0
     CHASIS_8TN: int = 0
-    CAMIONETA: int = 0
+    PICKUP_TRUCK: int = 0
 
 @dataclass
 class Orders:
